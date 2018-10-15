af3c90cc542c7da66847cf80e6e94527d476d096
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-15 23:31:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
    people_counter = 0
    
    data[season].each do |contestants_array|
-     total_age += contestants_array["age"].to_i
+     total_age += contestants_array["age"].to_f
      people_counter += 1
    end 
    
