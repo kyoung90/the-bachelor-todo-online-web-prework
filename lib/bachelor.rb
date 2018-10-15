3aa67cc7f988dac025e6c76b99a5c69e6730e3d5
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-15 23:22:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,22 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |season_info|
+   if season_info.has_value?("Winner")
+     winner_name = season_info["name"].split(" ")
+     return winner_name[0]
+   end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, contestants_array|
+    contestants_array.each do |contestant_info|
+      if contestant_info.has_value?(occupation)
+        return contestant_info["name"]
+      end 
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
